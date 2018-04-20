686030a11230225ed239d46eaa4aea2b39d46f70
matthewcarlreetz <matthewcarlreetz@gmail.com>
2018-04-20 15:00:57 -0500

    Added camera, far from done :)

--- src/components/AddLibrary.tsx
+++ src/components/AddLibrary.tsx
@@ -1,9 +1,17 @@
 import React, { Component } from "react";
-import { StyleSheet, Text } from "react-native";
 import { Library } from "../redux/library/types";
 import { StyleProvider, Container, Content, Root } from "native-base";
 import getTheme from "../../native-base-theme/components/index";
 import platform from "../../native-base-theme/variables/platform";
+import { RNCamera } from "react-native-camera";
+import {
+    AppRegistry,
+    Dimensions,
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    View
+} from "react-native";
 
 export interface ConnectProps {
     location: Position;
@@ -16,28 +24,60 @@ interface State {
 }
 
 export default class LibraryDetail extends Component<ConnectProps, State> {
+    camera: RNCamera;
+
     render() {
-        let location = "Add Library";
-        if (this.props.location != null) {
-            location = this.props.location.coords.latitude +
-                " <==> " + this.props.location.coords.longitude;
-        }
-
         return (
-            <Root>
-                <StyleProvider style={getTheme(platform)}>
-                    <Container>
-                        <Content>
-                            <Text>{location}
-                            </Text>
-                        </Content>
-                    </Container>
-
-                </StyleProvider>
-            </Root>
+            <View style={styles.container}>
+                <RNCamera
+                    ref={ref => {
+                        this.camera = ref;
+                    }}
+                    style={styles.preview}
+                    type={RNCamera.Constants.Type.back}
+                    flashMode={RNCamera.Constants.FlashMode.on}
+                    permissionDialogTitle={"Permission to use camera"}
+                    permissionDialogMessage={"We need your permission to use your camera phone"}
+                />
+                <View style={{ flex: 0, flexDirection: "row", justifyContent: "center", }}>
+                    <TouchableOpacity
+                        onPress={this.takePicture.bind(this)}
+                        style={styles.capture}
+                    >
+                        <Text style={{ fontSize: 14 }}> SNAP </Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
         );
+    }
+
+    takePicture() {
+        if (this.camera) {
+            this.camera.takePictureAsync({ quality: 0.5, base64: true })
+                .then((data) => console.log(data))
+                .catch((err: any) => console.error(err));
+        }
     }
 }
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: "column",
+        backgroundColor: "black"
+    },
+    preview: {
+        flex: 1,
+        justifyContent: "flex-end",
+        alignItems: "center"
+    },
+    capture: {
+        flex: 0,
+        backgroundColor: "#fff",
+        borderRadius: 5,
+        padding: 15,
+        paddingHorizontal: 20,
+        alignSelf: "center",
+        margin: 20
+    }
 });
