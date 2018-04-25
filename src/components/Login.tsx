41642b4a4c40a3fef781038743695aeb24bb4ab6
matthewcarlreetz <matthewcarlreetz@gmail.com>
2018-04-25 16:42:32 -0500

    Added lottie and a little fade animation

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { View, StyleSheet, Text, Image } from "react-native";
+import { View, StyleSheet, Text, Image, Animated } from "react-native";
 import { StyleProvider } from "native-base";
 import getTheme from "../../native-base-theme/components/index";
 import platform from "../../native-base-theme/variables/platform";
@@ -7,6 +7,7 @@ import LLButton from "./LLButton";
 import {
   Container, Header, Content, Form, Item, Input, Label, Icon, Button, H1, H3, Toast, Root
 } from "native-base";
+import Animation from "lottie-react-native";
 
 interface Props {
   email: string;
@@ -18,14 +19,36 @@ interface Props {
 interface State {
   email: string;
   password: string;
+  fadeOut: Animated.Value;
+  fadeIn: Animated.Value;
 }
 
 export default class LoginComponent extends Component<Props, State> {
+  private animationCheckedDone: any;
+
   constructor(props) {
     super(props);
     this.state = {
-      email: this.props.email, password: ""
+      email: this.props.email, password: "", fadeOut: new Animated.Value(1), fadeIn: new Animated.Value(0)
     };
+  }
+
+  componentDidUpdate() {
+    if (this.animationCheckedDone) {
+      this.animationCheckedDone.play();
+    }
+
+    if (this.props.loading) {
+      Animated.timing(this.state.fadeOut, {
+        toValue: 0.5,
+        duration: 300,
+      }).start();
+
+      Animated.timing(this.state.fadeIn, {
+        toValue: 1.0,
+        duration: 300,
+      }).start();
+    }
   }
 
   render() {
@@ -33,7 +56,7 @@ export default class LoginComponent extends Component<Props, State> {
       <Root>
         <StyleProvider style={getTheme(platform)}>
           <Container>
-            <Content style={styles.content}>
+            <View style={styles.content}>
               <Image source={require("../../assets/books.png")} style={styles.bookIcon} />
               <H1 style={{ fontWeight: "bold" }}>Littlest Library</H1>
               <Text style={styles.shareText}>Share the love!</Text>
@@ -46,7 +69,7 @@ export default class LoginComponent extends Component<Props, State> {
                     keyboardType="email-address"
                     // value={this.state.email}
                     value={"matt.reetz@zymo.io"}
-                    onChangeText={text => this.setState({ email: text })}
+                    onChangeText={email => this.setState({ email })}
                   />
                 </Item>
                 <Item floatingLabel style={styles.item}>
@@ -54,7 +77,7 @@ export default class LoginComponent extends Component<Props, State> {
                   <Input
                     value={"abcd1234"}
                     secureTextEntry
-                    onChangeText={text => this.setState({ password: text })}
+                    onChangeText={password => this.setState({ password })}
                   />
                 </Item>
               </Form>
@@ -70,13 +93,32 @@ export default class LoginComponent extends Component<Props, State> {
                   </Button>
                 </View>
               </View>
-              <LLButton
-                onPress={() => this.props.login(this.state.email, this.state.password)}
-                text="Login"
-                style={styles.button}
-                disabled={this.props.loading}
-              />
-            </Content>
+
+              <Animated.View style={{ opacity: this.state.fadeOut }}>
+                <LLButton
+                  onPress={() => this.props.login(this.state.email, this.state.password)}
+                  text="Login"
+                  style={styles.button}
+                  disabled={this.props.loading}
+                />
+              </Animated.View>
+              {
+                this.props.loading &&
+                <Animated.View style={{
+                  opacity: this.state.fadeIn,
+                  width: 327,
+                  height: 100, alignItems: "center", justifyContent: "center"
+                }}>
+                  <Animation
+                    ref={(animation: any) => {
+                      this.animationCheckedDone = animation;
+                    }}
+                    loop={true}
+                    source={require("../../assets/anim/loading.json")}
+                  />
+                </Animated.View>
+              }
+            </View>
           </Container>
         </StyleProvider>
       </Root>
@@ -86,6 +128,7 @@ export default class LoginComponent extends Component<Props, State> {
 
 const styles = StyleSheet.create({
   content: {
+    flexDirection: "column",
     paddingLeft: 24,
     marginRight: 24,
     marginTop: 48
@@ -120,5 +163,6 @@ const styles = StyleSheet.create({
   },
   buttonContainer: {
     flex: 1
-  }
+  },
+  loading: { resizeMode: "contain", height: 100, width: 300 }
 });
